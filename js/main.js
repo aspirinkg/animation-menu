43de7bca9510cd27eb5a3aaf1847d474a5eefc0b
aspirin <aspirin.kg@mail.ru>
2018-03-12 02:47:19 +0600

    menu animation - ready

---
+++ js/main.js
@@ -0,0 +1,36 @@
+ //велосипед какой то , но работает
+ $(document).ready(function(){
+	$('#toggleNavigation').on('click',(e)=>{
+		e.preventDefault();
+		
+		// убрать
+		$('body').addClass('hideScroll');
+		$('html').addClass('hideScroll');
+
+		// лень переписывать)
+		let boolClass = $('.navigation').attr('class') == 'navigation navigation-open' ? true : false;
+
+		if(boolClass){
+			
+			  $('.navigation').addClass('menu-close');
+
+			  // по завершению анимации закрытия удалить все классы , вроде точно подобрал
+			  setTimeout(() => {
+
+				$('.navigation').removeClass('navigation-open');
+				$('.navigation').removeClass('menu-close');
+				$('body').removeClass('hideScroll');
+				$('html').removeClass('hideScroll');
+
+			  }, 1500);
+
+		}
+		else{
+			//анимация открытия
+			$('.navigation').addClass('navigation-open');
+			// $('body').toggleClass('hideScroll');
+			// $('html').toggleClass('hideScroll');
+		}
+	})
+
+});
